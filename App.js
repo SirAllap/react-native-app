53253ab67b214ecb9b0c530a3f9107631ee09932
David PR <serallapdivad@gmail.com>
2023-12-26 18:14:16 +0000

    Init project

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView, StyleSheet, Text, View, Image } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <View >
+        <Text style={styles.main}>Open up App.js to start working on your appasdasd!</Text>
+        <StatusBar style="ligth" backgroundColor="#222222" barStyle="content-ligth" translucent={true} />
+        <Image source={require('./assets/adaptive-icon.png')} style={{
+          width: 100,
+          height: 100,
+          resizeMode: 'contain',
+          alignSelf: 'center',
+        }} />
+      </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#222222',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  main: {
+    color: 'white',
+  }
+})
