1844bcacaf9c3a14d22c60f0c2b45d506fb530b0
David PR <serallapdivad@gmail.com>
2023-12-27 17:11:34 +0000

    created new elements on the home screen

--- app/HomeScreen.tsx
+++ app/HomeScreen.tsx
@@ -1,38 +1,55 @@
 import React from 'react'
 import {
 	SafeAreaView,
-	StyleSheet,
 	Text,
 	View,
 	Image,
 	Button,
+	TextInput,
+	Pressable,
 } from 'react-native'
 import { HomeProps } from '../interfaces/NavigationInterfaces'
 import { styles } from '../styles/styles'
 
 const HomeScreen: React.FC<HomeProps> = ({ navigation }) => {
+	const [refNumber, setRefNumber] = React.useState<string>('')
+
 	return (
 		<SafeAreaView style={styles.container}>
-			<View style={styles.headerContainer}>
+			{/* <View style={styles.headerContainer}>
 				<Image
 					source={require('../assets/miranda_logo.png')}
-					style={{
-						width: 170,
-						height: 100,
-						resizeMode: 'contain',
-					}}
+					style={styles.imageStyle}
 				/>
-				<Text style={styles.text}>
-					Open up App.js to start working on your Application!
-				</Text>
-				<Button
-					title='CHECK IN'
+				<Pressable
+					onPress={() => navigation.navigate('InfoScreen')}
+					style={styles.navMenu}
+				>
+					<Text>Info</Text>
+				</Pressable>
+			</View> */}
+
+			<View style={styles.bodyContainer}>
+				<Text style={styles.inputLabel}>Booking reference number:</Text>
+				<TextInput
+					style={styles.inputStyle}
+					keyboardType='numeric'
+					onChangeText={setRefNumber}
+					placeholder='0000'
+					placeholderTextColor='#a5a4a4'
+					value={refNumber}
+					maxLength={4}
+				/>
+				<Pressable
 					onPress={() =>
-						navigation.navigate('InfoScreen', { name: 'David' })
+						navigation.navigate('CheckInScreen', {
+							checkInRef: refNumber,
+						})
 					}
-					color='rebeccapurple'
-					disabled={false}
-				/>
+					style={styles.buttonStyle}
+				>
+					<Text style={styles.buttonText}>CHECK IN</Text>
+				</Pressable>
 			</View>
 		</SafeAreaView>
 	)
