b2df8dde0e22c1215354b272d289696fe348b207
David PR <serallapdivad@gmail.com>
2023-12-28 16:13:47 +0000

    Created a new paramList

--- interfaces/NavigationInterfaces.ts
+++ interfaces/NavigationInterfaces.ts
@@ -1,15 +1,21 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import {
 	NativeStackNavigationProp,
 	NativeStackScreenProps,
 } from '@react-navigation/native-stack'
 
 export type RootStackParamList = {
+	Login: { email: string; password: string }
 	Home: undefined
 	CheckInScreen: { checkInRef: string }
 	InfoScreen: undefined
 }
 
 export type HomeProps = {
+	navigation: NativeStackNavigationProp<RootStackParamList, 'Home'>
+}
+
+export type LoginProps = {
 	navigation: NativeStackNavigationProp<RootStackParamList, 'Home'>
 }
 
